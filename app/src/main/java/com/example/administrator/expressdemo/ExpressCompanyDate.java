c5a99ca3efecb3bcc50ca752629457f03f60c759
Administrator <783719477@qq.com>
2017-02-22 21:04:45 +0800

    的

---
+++ app/src/main/java/com/example/administrator/expressdemo/ExpressCompanyDate.java
@@ -0,0 +1,33 @@
+package com.example.administrator.expressdemo;
+
+/**
+ * Created by Administrator on 2017/2/22.
+ */
+
+public class ExpressCompanyDate {
+
+
+    /**
+     * com : 顺丰
+     * no : sf
+     */
+
+    private String com;
+    private String no;
+
+    public String getCom() {
+        return com;
+    }
+
+    public void setCom(String com) {
+        this.com = com;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+}
